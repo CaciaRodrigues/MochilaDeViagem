73df1f17361275b835856ed4426b78b93342882c
Cacia Rodrigues <caciarodriguues@gmail.com>
2023-06-23 10:21:17 -0300

    Projeto Inicial + manipulação de forms com js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('novoItem');
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function criaElemento(nome, quantidade) {
+ console.log(nome);
+ console.log(quantidade);
+
+ const novoItem = document.createElement("li")
+}
