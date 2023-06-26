88e9956bd8355665daca3b126a4146a3a93ca170
Cacia Rodrigues <caciarodriguues@gmail.com>
2023-06-26 10:31:17 -0300

    Salvando itens no localStorage+melhorando o código

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById("lista");
+const itens = [];
+
+/* Evento de clique para envio do form */
 
 form.addEventListener('submit', (evento) => {
-    evento.preventDefault()
+    evento.preventDefault();
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = "";
+    quantidade.value = "";
 })
+
+/* Criação de elementos nos quais as informações do form serão armazenadas */
 
 function criaElemento(nome, quantidade) {
  const novoItem = document.createElement("li");
@@ -18,4 +29,17 @@ function criaElemento(nome, quantidade) {
  novoItem.innerHTML += nome;  
 
  lista.appendChild(novoItem);
+
+ /* Salvando os elementos no localStorage */
+
+ const itemAtual = {
+    "nome" : nome,
+    "quantidade": quantidade
+
+ }
+
+ itens.push(itemAtual);
+
+ localStorage.setItem("item", JSON.stringify(itens));
+ 
 }
