1a0a2be0a67015a99215c70b2ca6fe554ca1e9c1
Cacia Rodrigues <caciarodriguues@gmail.com>
2023-06-28 19:10:14 -0300

    Botão deletar + refatoração do código

--- js/main.js
+++ js/main.js
@@ -27,10 +27,10 @@ form.addEventListener('submit', (evento) => {
     
     atualizaElemento(itemAtual);
 
-    itens[existe.id] = itemAtual;
+    itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
   } else {
-    /* Se o elemento não existe cria o elemento e adciona um id baseado no tamanho do array*/
-    itemAtual.id = itens.length;
+    /* Se o elemento não existe cria o elemento e adciona um id baseado no tamanho do array -melhorado*/
+    itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
 
     criarElemento(itemAtual);
 
@@ -57,8 +57,9 @@ function criarElemento(item) {
   novoItem.appendChild(numItem);
   novoItem.innerHTML += item.nome;
 
-  lista.appendChild(novoItem);
+  novoItem.appendChild(botaoDeletar(item.id));
 
+  lista.appendChild(novoItem);
 }
 
 /*Função da Atualização do elemento */
@@ -66,4 +67,28 @@ function criarElemento(item) {
 function atualizaElemento(item) {
 document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
   // 
+}
+
+/* Função para deletar o item */
+
+function botaoDeletar(id) {
+  /*criando o botão de deletar */
+  const elementoBotao = document.createElement("button");
+  elementoBotao.innerText = "X";
+
+  /*Apontando para o que botão precisa deletar ao ser clicado */
+
+  elementoBotao.addEventListener('click', function() {
+    deletarElemento(this.parentNode, id);
+  })
+
+  return elementoBotao;
+}
+
+function deletarElemento(tag, id) {
+  tag.remove();
+
+  itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+  localStorage.setItem('itens', JSON.stringify(itens));
 }
